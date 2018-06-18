593c9344325f5f0c0af1406194c94921a6f234a0
cat805 <nguyencatherine2@gmail.com>
2018-06-18 16:22:59 -0400

    turn left

--- go_forward.py
+++ go_forward.py
@@ -1,8 +1,12 @@
 from picar import back_wheels
+from picar import front_wheels
 import picar
+import time
+import keyboard
 
 picar.setup()
 
+fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels(db='config')
 forward_speed = 100
 
@@ -13,9 +17,16 @@ def start_follower():
 		print "start_follow"
 		bw.speed = forward_speed
 
+        #if turn left key was pressed
+		if keyboard.is_pressed('q'):
+			left_turn()
+
 def stop():
 	bw.stop()
 	#fw.turn_straight()
+
+def left_turn():
+	fw.turn_left()
 
 if __name__ == '__main__':
 	try:
