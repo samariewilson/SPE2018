734a76038854c684b1241e5beda33759d62e27f4
cat805 <nguyencatherine2@gmail.com>
2018-06-19 15:45:49 -0400

    pygame

---
+++ go_pygame.py
@@ -0,0 +1,37 @@
+rom picar import back_wheels
+from picar import front_wheels
+import picar
+import pygame
+
+picar.setup()
+
+fw = front_wheels.Front_Wheels(db='config')
+bw = back_wheels.Back_Wheels(db='config')
+
+forward_speed = 100
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left_turn()
+            if event.key == pygame.K_q:
+                stop()
+            
+
+
+def stop():
+    bw.stop()
+
+def left_turn():
+	fw.turn_left()
+
+def right_turn():
+	fw.turn_right()
+
+if __name__ == '__main__':
+	try:
+		start_follower()
+	except KeyboardInterrupt:
+		stop()
