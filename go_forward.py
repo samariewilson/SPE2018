95c034513f2b1fe703ff44463932debecd7b401a
cat805 <nguyencatherine2@gmail.com>
2018-06-19 15:08:12 -0400

    go forward

--- go_forward.py
+++ go_forward.py
@@ -8,25 +8,37 @@ picar.setup()
 fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels(db='config')
 
+forward_speed = 100
 
 def start_follower():
 
     while True:
         print("start_follow")
-		# if turn left key was pressed
-        if keyboard.is_pressed('l'):
+
+        if keyboard.is_pressed('a'):
             left_turn()
-		elif keyboard.is_pressed('q'):
+		if keyboard.is_pressed('d'):
+			right_turn()
+		if keyboard.is_pressed('q'):
 			stop()
-        else:
+			exit()
+		if keyboard.is_pressed('s'):
+			bw.speed = forward_speed
+			bw.backward()
+        elif keyboard.is_press('w'):
             bw.speed = forward_speed
+			bw.forward()
+		else:
+			stop()
 
 def stop():
     bw.stop()
-	#fw.turn_straight()
 
 def left_turn():
 	fw.turn_left()
+
+def right_turn():
+	fw.turn_right()
 
 if __name__ == '__main__':
 	try:
