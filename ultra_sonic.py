05def66bc75c002c961375348e22768d7c3f2730
cat805 <nguyencatherine2@gmail.com>
2018-06-20 11:00:00 -0400

    ultra sonic go forward outside

--- ultra_sonic.py
+++ ultra_sonic.py
@@ -84,10 +84,11 @@ if __name__ == '__main__':
 	UA = Ultrasonic_Avoidance(17)
 	threshold = 10
 	while True:
+        bw.speed = forward_speed
+        bw.forward()
+        time.sleep(0.2)
 		distance = UA.get_distance()
 		status = UA.less_than(threshold)
-
-
         if distance != -1:
             print 'distance', distance, 'cm'
             bw.speed = forward_speed
@@ -100,7 +101,6 @@ if __name__ == '__main__':
             print "Less than %d" % threshold
             bw.speed = forward_speed
             bw.forward()
-
         elif status == 0:
             print "Over %d" % threshold
             bw.stop()
