8cb63b00d5cc312f02d681acb1ef322e94c605a3
cat805 <nguyencatherine2@gmail.com>
2018-06-20 15:27:56 -0400

    web socket

---
+++ server.py
@@ -0,0 +1,49 @@
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+import sys
+
+port = 9876
+
+class SimpleEcho(WebSocket):
+
+    def handleMessage(self):
+        print(self.data)
+        if self.data == "up":
+            bw.speed = forward_speed
+            bw.backward()
+            straight_turn()
+        elif self.data == "down":
+            bw.speed = forward_speed
+            bw.forward()
+            straight_turn()
+        elif self.data == "right":
+            right_turn()
+            bw.speed = forward_speed
+        elif self.data == "left":
+            left_turn()
+            bw.speed = forward_speed
+        elif self.data == "straight":
+            straight_turn()
+        else:
+            stop()
+
+
+    def stop():
+        bw.stop()
+
+    def left_turn():
+    	fw.turn(82)
+
+    def straight_turn():
+        fw.turn(102)
+    def right_turn():
+    	fw.turn(12)
+
+    def handleConnected(self):
+        print(self.address, 'connected')
+
+    def handleClose(self):
+        print(self.address, 'closed')
+
+server = SimpleWebSocketServer('', port, SimpleEcho)
+server.serveforever()
