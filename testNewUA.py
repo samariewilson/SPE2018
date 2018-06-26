9a047e1fe016184c8c289a4e83edc06ffb5b1336
Jo1604 <40275167+Jo1604@users.noreply.github.com>
2018-06-26 14:37:09 -0400

    test distance prints of new UA

--- testNewUA.py
+++ testNewUA.py
@@ -1,43 +1,57 @@
+
+class Ultrasonic_Avoidance(object):
+    timeout = 0.05
+
 def __init__(self, trig, echo):
-      self.trig = trig
-      self.echo = echo
-      GPIO.setmode(GPIO.BOARD)
+    self.trig = trig
+    self.echo = echo
+    GPIO.setmode(GPIO.BOARD)
 
-  def distance(self):
-      GPIO.setup(self.trig, GPIO.OUT)
-      GPIO.setup(self.echo, GPIO.IN)
+def distance(self):
+    GPIO.setup(self.trig, GPIO.OUT)
+    GPIO.setup(self.echo, GPIO.IN)
 
-      GPIO.output(self.trig, GPIO.LOW)
+    GPIO.output(self.trig, GPIO.LOW)
 
-      print("Waiting for sensor to settle")
-      time.sleep(2)
-      print("Calculating distance")
+    print("Waiting for sensor to settle")
+    time.sleep(2)
+    print("Calculating distance")
 
-      GPIO.output(self.trig, GPIO.HIGH)
-      time.sleep(0.00001)
-      GPIO.output(self.trig, GPIO.LOW)
+    GPIO.output(self.trig, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(self.trig, GPIO.LOW)
 
-      timeout_start = time.time()
-      while GPIO.input(self.echo) == 0:
-          pulse_start_time = time.time()
-          if pulse_start - timeout_start > self.timeout:
-              return -1
-      while GPIO.input(self.echo) == 1:
-          pulse_end_time = time.time()
-          if pulse_start - timeout_start > self.timeout:
-              return -1
+    timeout_start = time.time()
+    while GPIO.input(self.echo) == 0:
+        pulse_start_time = time.time()
+        if pulse_start - timeout_start > self.timeout:
+            return -1
+    while GPIO.input(self.echo) == 1:
+        pulse_end_time = time.time()
+        if pulse_start - timeout_start > self.timeout:
+            return -1
       # Computes distance
-      if pulse_start != 0 and pulse_end != 0:
-          pulse_duration = pulse_end - pulse_start
-          distance = pulse_duration * 100 * 343.0 / 2
-          distance = int(distance)
+        if pulse_start != 0 and pulse_end != 0:
+            pulse_duration = pulse_end - pulse_start
+            distance = pulse_duration * 100 * 343.0 / 2
+            distance = int(distance)
       # print 'start = %s'%pulse_start,
       # print 'end = %s'%pulse_end
-          if distance >= 0:
-              print (distance)
-          else:
-              print(-1)
-      else:
+            if distance >= 0:
+                return distance
+            else:
+                return -1
+        else:
           # print 'start = %s'%pulse_start,
           # print 'end = %s'%pulse_end
-          print(-1)
+            return -1
+
+
+if __name__ == '__main__':
+    trig = 20
+    echo = 16
+    def make_Ultrasonic_Avoidance(trig, echo):
+        senor = Ultrasonic_Avoidance(trig, echo)
+        return sensor   
+    threshold = 10
+    print(distance)
