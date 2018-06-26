020244a43edd5ee7dceef6332e6016c5c64e9bd1
cat805 <nguyencatherine2@gmail.com>
2018-06-26 15:25:32 -0400

    new server

--- server3.py
+++ server3.py
@@ -1,35 +1,43 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
 
-TRIG = 20
-ECHO = 16
+class Ultrasonic_Avoidance:
 
-print "Distance Measurement In Progress"
+    def __init__(self, trig, echo):
+        GPIO.setmode(GPIO.BCM)
+        self.TRIG = trig
+        self.ECHO = echo
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+    def distance(self):
 
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
+        print "Distance Measurement In Progress"
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+        GPIO.setup(self.TRIG,GPIO.OUT)
+        GPIO.setup(self.ECHO,GPIO.IN)
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+        GPIO.output(self.TRIG, False)
+        print "Waiting For Sensor To Settle"
+        time.sleep(2)
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+        GPIO.output(self.TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(self.TRIG, False)
 
-pulse_duration = pulse_end - pulse_start
+        while GPIO.input(self.ECHO)==0:
+          pulse_start = time.time()
 
-distance = pulse_duration * 17150
+        while GPIO.input(self.ECHO)==1:
+          pulse_end = time.time()
 
-distance = round(distance, 2)
+        pulse_duration = pulse_end - pulse_start
 
-print "Distance:",distance,"cm"
+        distance = pulse_duration * 17150
 
-GPIO.cleanup()
+        distance = round(distance, 2)
+
+        return distance
+
+if __name__ == '__main__':
+
+    sensor = Ultrasonic_Avoidance(20, 16)
+    sensor.distance()
