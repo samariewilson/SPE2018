9bb87e52fc81951d78342558b68344aa696b7124
Jo1604 <40275167+Jo1604@users.noreply.github.com>
2018-06-26 14:12:25 -0400

    Test out merge code

--- server_new_ultrasound.py
+++ server_new_ultrasound.py
@@ -17,8 +17,9 @@ forward_speed = 80
 
 port = 9876
 
-close_to_wall = Value ('b', False)
-emergency_backup = Value ('b', False)
+close_to_wall = Value('b', False)
+emergency_backup = Value('b', False)
+
 
 class SimpleEcho(WebSocket):
 
@@ -50,11 +51,15 @@ class SimpleEcho(WebSocket):
 def stop():
     bw.stop()
 
+
 def left_turn():
     fw.turn(82)
 
+
 def straight_turn():
     fw.turn(102)
+
+
 def right_turn():
     fw.turn(122)
 
@@ -64,53 +69,53 @@ def right_turn():
     def handleClose(self):
         print(self.address, 'closed')
 
+
 class Ultrasonic_Avoidance(object):
     timeout = 0.05
 
     def __init__(self, trig, echo):
-		self.trig = trig
+        self.trig = trig
         self.echo = echo
         GPIO.setmode(GPIO.BOARD)
 
     def distance(self):
+        GPIO.setup(self.trig, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
 
-      GPIO.setup(self.trig, GPIO.OUT)
-      GPIO.setup(self.echo, GPIO.IN)
+        GPIO.output(self.trig, GPIO.LOW)
 
-      GPIO.output(self.trig, GPIO.LOW)
+        print("Waiting for sensor to settle")
+        time.sleep(2)
+        print("Calculating distance")
 
-      print "Waiting for sensor to settle"
-      time.sleep(2)
-      print "Calculating distance"
+        GPIO.output(self.trig, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(self.trig, GPIO.LOW)
 
-      GPIO.output(self.trig, GPIO.HIGH)
-      time.sleep(0.00001)
-      GPIO.output(self.trig, GPIO.LOW)
-
-      timeout_start = time.time()
-      while GPIO.input(self.echo)==0:
+        timeout_start = time.time()
+        while GPIO.input(self.echo) == 0:
             pulse_start_time = time.time()
             if pulse_start - timeout_start > self.timeout:
                 return -1
-      while GPIO.input(self.echo)==1:
+        while GPIO.input(self.echo) == 1:
             pulse_end_time = time.time()
             if pulse_start - timeout_start > self.timeout:
                 return -1
         # Computes distance
         if pulse_start != 0 and pulse_end != 0:
-        	pulse_duration = pulse_end - pulse_start
-        	distance = pulse_duration * 100 * 343.0 /2
-        	distance = int(distance)
-        	#print 'start = %s'%pulse_start,
-        	#print 'end = %s'%pulse_end
-        	if distance >= 0:
-        		return distance
-        	else:
-        		return -1
-        else :
-        	#print 'start = %s'%pulse_start,
-        	#print 'end = %s'%pulse_end
-        	return -1
+            pulse_duration = pulse_end - pulse_start
+            distance = pulse_duration * 100 * 343.0 /2
+            distance = int(distance)
+        # print 'start = %s'%pulse_start,
+        # print 'end = %s'%pulse_end
+            if distance >= 0:
+                return distance
+            else:
+                return -1
+        else:
+            # print 'start = %s'%pulse_start,
+            # print 'end = %s'%pulse_end
+            return -1
 
 # Computes average distances
     def get_distance(self, mount = 10):
