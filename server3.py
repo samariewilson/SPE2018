c9915b72b05c11a80372f8e12dc524c9da4e8e5b
cat805 <nguyencatherine2@gmail.com>
2018-06-26 15:27:28 -0400

    new server

--- server3.py
+++ server3.py
@@ -17,7 +17,7 @@ class Ultrasonic_Avoidance:
 
         GPIO.output(self.TRIG, False)
         print "Waiting For Sensor To Settle"
-        time.sleep(2)
+        #time.sleep(2)
 
         GPIO.output(self.TRIG, True)
         time.sleep(0.00001)
