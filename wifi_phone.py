faa103dd1abf7fa5174fb82135d81eaaf06bcb48
cat805 <nguyencatherine2@gmail.com>
2018-06-27 14:55:52 -0400

    while true phone

--- wifi_phone.py
+++ wifi_phone.py
@@ -2,19 +2,20 @@ from sys import exit
 import logging
 import iwlist
 
-content = iwlist.scan(interface='wlan0')
-cells = iwlist.parse(content)
-signal = 0
+while (True):
+    content = iwlist.scan(interface='wlan0')
+    cells = iwlist.parse(content)
+    signal = 0
 
-for i in cells:
 
-    if i["essid"].startswith("Catherine"):
+    for i in cells:
 
-        mac = i["mac"]
-        signal = i["signal_level_dBm"]
-        age = 0
-        break
+        if i["essid"].startswith("Catherine"):
 
-    
+            mac = i["mac"]
+            signal = i["signal_level_dBm"]
+            age = 0
+            break
 
-print signal
+    print signal
+    sleep (2)
