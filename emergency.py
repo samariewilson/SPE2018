d904007e35dee5db3b9f74e8bcd0b0b113dcf363
cat805 <nguyencatherine2@gmail.com>
2018-06-28 11:26:04 -0400

    turn

--- emergency.py
+++ emergency.py
@@ -2,5 +2,7 @@ from picar import back_wheels
 from picar import front_wheels
 import picar
 
+fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels(db='config')
-bw.stop() 
+fw.turn(102)
+bw.stop()
