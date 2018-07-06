b9210539eb21a44e7502cb1f8baac6f9a9d644c9
SamarieW <samariew@princeton.edu>
2018-07-06 11:55:18 -0400

    fix thread

--- socket_lol.py
+++ socket_lol.py
@@ -4,7 +4,7 @@ import sys
 import threading
 
 host = ''
-port = 9876
+port = 5555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
@@ -27,7 +27,7 @@ def threaded_client(conn):
 
 while True:
     conn, addr = s.accept()
-    print('connected to: ' + addr[0] + ':' + str(addr[1]))
+    print('connected to: ' + addr[0] + ': ' + str(addr[1]))
 
     start_new_thread(threaded_client,(conn,))
 
