bfe86c8f6a7039223aeae0ff8523f79f46856a3b
Jo1604 <40275167+Jo1604@users.noreply.github.com>
2018-07-09 12:11:37 -0400

    Johana's test version

--- testWS.py
+++ testWS.py
@@ -1,15 +1,33 @@
-# Echo server program
 import socket
+import sys
+from _thread import *
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
+host = ''
+port = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+s.listen(5)
+print('Waiting for a connection.')
+def threaded_client(conn):
+    conn.send(str.encode('CAT Welcome, type your info\n'))
+
+    while True:
+        data = conn.recv(2048)
+        reply = 'Server output: '+ data.decode('utf-8')
+        if not data:
+            break
+        conn.sendall(str.encode(reply))
+    conn.close()
+
+
+while True:
+
     conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+    print('connected to: '+addr[0]+':'+str(addr[1]))
+
+    start_new_thread(threaded_client,(conn,))
