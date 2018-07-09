f94df48a76de842d3194183f6846418b88dabb94
cat805 <nguyencatherine2@gmail.com>
2018-07-09 16:01:17 -0400

    sensors

--- threeFunctions.py
+++ threeFunctions.py
@@ -252,6 +252,7 @@ def distanceLoop():
 
 
 server = SimpleWebSocketServer('', port, SimpleEcho)
+server.serveforever()
 p1 = Process(target = server.serveforever)
 p2 = Process(target = distanceLoop)
 p2.start()
