6b4bc671bfb2bb9a03e868f28b9ef1a1253a15a1
SamarieW <samariew@princeton.edu>
2018-07-16 10:56:35 -0400

    yay

--- where_am_i.py
+++ where_am_i.py
@@ -1,59 +1,6 @@
 import time
 import numpy as np
 
-
-def update_time():
-    start = 0
-    end = 0
-    seconds = 0
-    for i in range(5):
-        start = time.time()
-        time.sleep(.5)
-        end = time.time()
-        seconds = end - start
-        return seconds
-
-
-def update_x(x_list):
-    x = x_list
-    speed = 0.5488                        # meters per second at speed 90
-    seconds = update_time()
-    distance = speed * seconds
-    random = np.random.randint(2)
-    last_place = x[-1]
-    distance = np.sin(20) * distance      # x distance traveled
-
-    if random == 0:                       # if left arrow is pressed
-        distance = np.sin(20) * distance
-        x.append(last_place - distance)
-    elif random == 1:                     # if right arrow is pressed
-        distance = np.sin(20) * distance
-        x.append(last_place + distance)
-
-    return x
-
-
-def update_y(y_list):
-    y = y_list
-    speed = 0.5488                        # meters per second at speed 90
-    seconds = update_time()
-    distance = speed * seconds
-    random = np.random.randint(4)
-    last_place = y[-1]
-
-    if random == 0:                       # if down arrow is pressed
-        y.append(last_place - distance)
-    elif random == 1:                     # if up arrow is pressed
-        y.append(last_place + distance)
-    elif random == 2:                     # if left arrow is pressed
-        distance = np.cos(20) * distance  # y distance traveled
-        y.append(last_place + distance)
-    elif random == 3:                     # if right arrow is pressed
-        distance = np.cos(20) * distance  # y distance traveled
-        y.append(last_place + distance)
-
-    return y
-
                                           # returns next coordinate of car
 def get_point(angles, time, direction, x, y):
     speed = 0.5488                        # meters per seconds at speed 90
@@ -97,6 +44,7 @@ def get_angle(angles, time, direction):
 
     angles.append(angle)
     return angle
+
 
 if __name__ == '__main__':
     x = [1, 2, 3, 4]
