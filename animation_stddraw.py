3aff17718fd463591e22769888462f569c024424
cat805 <nguyencatherine2@gmail.com>
2018-07-17 10:42:44 -0400

    manager

--- animation_stddraw.py
+++ animation_stddraw.py
@@ -35,7 +35,7 @@ class SimpleEcho(WebSocket):
         #print(strength)
 
 def mapper():
-    x2, y2, strength = get_point(angles, time, direction, x, y, strength)
+    x2, y2, strength2 = get_point(angles, time, direction, x, y, strength)
     max = 90
     increment = 10
     #strength = s
