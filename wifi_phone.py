09197f32d3276b7ed7619a53c42d9c53a704480a
cat805 <nguyencatherine2@gmail.com>
2018-07-18 16:48:37 -0400

    phone

--- wifi_phone.py
+++ wifi_phone.py
@@ -4,23 +4,21 @@ import iwlist
 import time
 
 def strength():
-    while (True):
-        content = iwlist.scan(interface='wlan0')
-        cells = iwlist.parse(content)
-        signal = 0
+while (True):
+    content = iwlist.scan(interface='wlan0')
+    cells = iwlist.parse(content)
+    signal = 0
 
 
-        for i in cells:
+    for i in cells:
 
-            if i["essid"].startswith("Catherine"):
+        if i["essid"].startswith("Catherine"):
 
-                mac = i["mac"]
-                signal = i["signal_level_dBm"]
-                age = 0
-                break
-
-        print signal
-
-        return signal
+            mac = i["mac"]
+            signal = i["signal_level_dBm"]
+            age = 0
+            break
+    return signal
     #time.sleep (2)
-strength()
+while True:
+    print strength()
